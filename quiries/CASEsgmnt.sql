b1db3f21aa699ce3b52f1f08ba6f0feca5c827fa
kamilaharutyunyan <kamilaharutyunyan.a@gmail.com>
2026-02-15 12:16:04 +0400

    updating the pazor

--- quiries/CASEsgmnt.sql
+++ quiries/CASEsgmnt.sql
@@ -3,7 +3,7 @@ CASE
 WHEN AVG(total_sales) > 252 THEN 'greater than avrg'
 WHEN AVG(total_sales) = 252 THEN 'avrg' 
 WHEN AVG(total_sales) < 252 THEN 'less than avrg' 
-ELSE 'PAZOR'
+ELSE 'WARNING'
 END AS sales_segment
 FROM sales_analysis
 Group by customer_name;
